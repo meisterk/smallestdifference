d538b7f24f73890a5c7a2b1f40169ba09c1a569e
Franz Kohnle <franz@kohnlehome.de>
2020-04-22 13:13:04 +0200

    Add class

---
+++ src/main/java/de/kohnlehome/SmallestDifference.java
@@ -0,0 +1,8 @@
+package de.kohnlehome;
+
+public class SmallestDifference implements ISmallestDifference {
+    @Override
+    public int smallestDifference(int[] array1, int[] array2) {
+        return 0;
+    }
+}
